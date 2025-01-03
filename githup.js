d240bea217973232f4599e1d45ade55243839222
reshmayadav40 <reshmayadav24@navgurukul.org>
2025-01-03 17:53:56 +0530

    first commit

--- githup.js
+++
@@ -1,5 +0,0 @@
-const fruits = ["Apple", "Banana", "Cherry", "Date"];
-
-for (let i = 0; i < fruits.length; i++) {
-  console.log(fruits[i]);
-}
---
+++ script.js
@@ -0,0 +1,4 @@
+function reshma(a,b){
+    return a+b;
+}
+console.log(reshma(2,4));
